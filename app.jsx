796f457ae843656a1b206b4fe66845a209d01133
Digitexmedia <150277877+Digitexmedia@users.noreply.github.com>
2025-06-11 02:06:48 +0300

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+    <div className="font-sans bg-gray-100 min-h-screen">
+      <Home />
+    </div>
+  );
+}
